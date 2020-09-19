fa52948b055dee09d6cbd3686ac966b71f80ac2e
Luiz Fernando <lfluiz.lf@gmail.com>
2020-09-19 11:18:34 -0300

    Incluindo Service Crud

--- hackaton-teris/src/app/app.module.ts
+++ hackaton-teris/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjetoCardComponent } from './components/projeto-card/projeto-card.component';
 import { ProjetoPageComponent } from './components/projeto-page/projeto-page.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProjetoPageComponent } from './components/projeto-page/projeto-page.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
