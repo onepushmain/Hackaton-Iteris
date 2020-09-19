51747d8e44ee7acb5b816897bd6ce2264d56880b
Luiz Fernando <lfluiz.lf@gmail.com>
2020-09-19 13:23:15 -0300

    aplicando routing

--- hackaton-teris/src/app/app-routing.module.ts
+++ hackaton-teris/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjetoPageComponent } from './components/projeto-page/projeto-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: ' ', component: ProjetoPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
